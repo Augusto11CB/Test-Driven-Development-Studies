68b891855e223159c32a2086fd5f9ea626486f5f
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-12-28 12:17:13 -0300

    Adding test databaseIsUsedIfDataIsPresent

---
+++ TDD-Project-ISBN-Calculator/Isbn-Calculator/test/com/isbn/calculator/StockManagerTest.java
@@ -0,0 +1,37 @@
+package com.isbn.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class StockManagerTest {
+
+    private ExternalISBNDataService dataService;
+    private StockManager stockManager;
+    private Book book;
+
+    @BeforeEach
+    private void init() {
+        this.dataService = mock(ExternalISBNDataService.class);
+        this.stockManager = new StockManager();
+        this.book = new Book("0140177396", "abc", "abc");
+    }
+
+    @Test
+    void checkInvalid10DigitsISBN() {
+
+        when(dataService.lookup(any())).thenReturn(book);
+
+        stockManager.setDataBaseService(dataService);
+        stockManager.setService(dataService);
+
+        String isbn = "0140177396";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+
+        verify(dataService, atMostOnce()).lookup(anyString());
+
+    }
+}
